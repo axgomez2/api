7c034f7ba28ce2510a51238186e5ef0cf6e49996
axgomez2 <ax.gomez2@gmail.com>
2025-07-13 19:48:40 -0300

    inserção do market analise

---
+++ app/Services/MarketAnalysisService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class MarketAnalysisService
+{
+    /**
+     * Create a new service instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    
+    /**
+     * Perform market analysis.
+     */
+    public function analyze()
+    {
+        // Implementation will go here
+        return true;
+    }
+}
