77ed487c0690c1166144f6878b5c746995b1f9cd
axgomez2 <ax.gomez2@gmail.com>
2025-07-25 15:23:53 -0300

    remoção api v2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,6 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            Route::prefix('api/v2')
-                ->middleware('api')
-                ->group(base_path('routes/api_v2.php'));
-        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         // Registrar seu middleware com um alias simples
