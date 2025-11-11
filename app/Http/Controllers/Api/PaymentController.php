3dfb39b3ef03487e6083556f66c5f31bf13ff5d2
axgomez2 <ax.gomez2@gmail.com>
2025-11-11 01:43:56 -0300

    correção namespace

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Order;
+use App\Models\Cart;
 use App\Models\ShippingQuote;
 use App\Models\ShippingLabel;
 use App\Models\OrderStatus;
